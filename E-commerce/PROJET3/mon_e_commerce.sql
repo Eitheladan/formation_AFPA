511a3a6285624ff044c42c1b97670c2abf2499f2
29010-33-07 <29010-33-07@exchange.ad.afpanet>
2021-10-14 16:57:56 +0200

    ajout

--- E-commerce/PROJET3/mon_e_commerce.sql
+++ E-commerce/PROJET3/mon_e_commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : ven. 08 oct. 2021 à 09:54
+-- Généré le : jeu. 14 oct. 2021 à 14:57
 -- Version du serveur : 5.7.33
 -- Version de PHP : 7.4.19
 
@@ -28,12 +28,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `commande` (
-  `id_commande` int(11) NOT NULL,
-  `ref_commande` varchar(50) COLLATE utf8mb4_bin NOT NULL,
-  `date_commande` date NOT NULL,
-  `total_commande` int(11) NOT NULL,
-  `id_client` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `id_commande` int(250) NOT NULL,
+  `qte` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `id_produit`, `id_commande`, `qte`) VALUES
+(5, 9, 5, 8),
+(6, 9, 5, 8),
+(7, 9, 6, 2),
+(8, 10, 6, 5),
+(9, 10, 6, 3),
+(10, 9, 7, 1),
+(11, 10, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -45,6 +57,41 @@ CREATE TABLE `comporter` (
   `id_produit` int(11) NOT NULL,
   `id_commande` int(11) NOT NULL,
   `quantite` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `list_commande`
+--
+
+CREATE TABLE `list_commande` (
+  `id_commande` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `list_commande`
+--
+
+INSERT INTO `list_commande` (`id_commande`, `id_utilisateur`) VALUES
+(6, 4),
+(7, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+
+CREATE TABLE `panier` (
+  `id` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `ref` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `qte` int(11) NOT NULL,
+  `prix` float NOT NULL,
+  `total` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
@@ -66,12 +113,12 @@ CREATE TABLE `produit` (
 --
 
 INSERT INTO `produit` (`id_produit`, `ref_produit`, `nom`, `prix_unitaire`, `img`) VALUES
-(1, 'TEST1', 'test', 10, 'france.jfif'),
-(2, 'TEST2', 'CHOUNGER', 15, 'finlande.jpg'),
-(3, 'TEST3', 'BOULCHA', 3, 'venezuela.jpg'),
-(4, 'TEST4', 'MOUJER', 8, 'marocaine.jpg'),
-(5, 'TEST5', 'BREOLORE', 25, NULL),
-(6, 'TEST1', 'jgjgjg', 35, NULL);
+(8, 'PARF001_100', 'GABRIELLE CHANEL 100ml', 65, 'gabriellechanel.jfif'),
+(9, 'PARF002_100', 'IRRESISTIBLE 100ml', 75, 'irresistible.jfif'),
+(10, 'PARF003_100', 'LA VIE EST BELLE 100ml', 55, 'lavieestbelle.jfif'),
+(11, 'PARF004_100', 'LIBRE100ml', 65, 'libre.jfif'),
+(12, 'PARF005_100', 'NINA 100ml', 78, 'nina.jfif'),
+(13, 'PARF006_100', 'ORMETA 100ml', 71, 'omerta.jfif');
 
 -- --------------------------------------------------------
 
@@ -125,8 +172,7 @@ INSERT INTO `utilisateur` (`id_client`, `nom`, `prenom`, `email`, `entreprise`, 
 -- Index pour la table `commande`
 --
 ALTER TABLE `commande`
-  ADD PRIMARY KEY (`id_commande`),
-  ADD KEY `commande_utilisateur_FK` (`id_client`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `comporter`
@@ -134,6 +180,19 @@ ALTER TABLE `commande`
 ALTER TABLE `comporter`
   ADD PRIMARY KEY (`id_produit`,`id_commande`),
   ADD KEY `comporter_commande0_FK` (`id_commande`);
+
+--
+-- Index pour la table `list_commande`
+--
+ALTER TABLE `list_commande`
+  ADD PRIMARY KEY (`id_commande`);
+
+--
+-- Index pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `PASSER` (`id_client`);
 
 --
 -- Index pour la table `produit`
@@ -163,13 +222,25 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `list_commande`
+--
+ALTER TABLE `list_commande`
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `panier`
+--
+ALTER TABLE `panier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `role`
@@ -188,17 +259,17 @@ ALTER TABLE `utilisateur`
 --
 
 --
--- Contraintes pour la table `commande`
---
-ALTER TABLE `commande`
-  ADD CONSTRAINT `commande_utilisateur_FK` FOREIGN KEY (`id_client`) REFERENCES `utilisateur` (`id_client`);
-
---
 -- Contraintes pour la table `comporter`
 --
 ALTER TABLE `comporter`
-  ADD CONSTRAINT `comporter_commande0_FK` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`),
+  ADD CONSTRAINT `comporter_commande0_FK` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`),
   ADD CONSTRAINT `comporter_produit_FK` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id_produit`);
+
+--
+-- Contraintes pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD CONSTRAINT `PASSER` FOREIGN KEY (`id_client`) REFERENCES `utilisateur` (`id_client`);
 
 --
 -- Contraintes pour la table `utilisateur`
